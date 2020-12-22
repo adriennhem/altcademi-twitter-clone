7e1892e70c707495e1f204d372f9693e01fe6f37
adrien nhem <adrien.nhem@gmail.com>
2020-12-22 11:45:56 +0000

    update session model

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,13 @@
 class Session < ApplicationRecord
+    belongs_to :user
 
+    validates :user_id, presence: true
+
+    before_validation :generate_token 
+
+    private 
+
+    def generate_token
+        self.token = SecureRandom.urlsafe_base64
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :sessions
+
     validates :email, length: { minimum: 5, maximum: 500 }, presence: true, uniqueness: true
     validates :username, length: { minimum: 3, maximum: 64 }, presence: true, uniqueness: true
     validates :password, length: { minimum: 8, maximum: 64 }, presence: true
