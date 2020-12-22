612b0eff61431e40b13a37bd79314ea94630f927
adrien nhem <adrien.nhem@gmail.com>
2020-12-22 12:20:06 +0000

    create POST session endpoint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,27 @@
 class SessionsController < ApplicationController
+    def create 
+        @user = User.find_by(username: params[:user][:username])
 
+        if @user and BCrypt::Password.new(@user.password) == params[:user][:password]
+            session = @user.sessions.create
+            cookies.permanent.signed[:twitter_session_token] = {
+                value: session.token,
+                httponly: true
+            }
+
+            render json: {
+                success: true 
+            }
+        else
+            render json: {
+                success: false
+            }
+        end
+    end 
+
+    def authenticated
+    end
+
+    def destroy
+    end
 end
