351140e5127da5b097f3d39018f43d38dc3d2bf8
adrien nhem <adrien.nhem@gmail.com>
2020-12-22 11:31:51 +0000

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    validates :email, length: { minimum: 5, maximum: 500 }, presence: true, uniqueness: true
+    validates :username, length: { minimum: 3, maximum: 64 }, presence: true, uniqueness: true
+    validates :password, length: { minimum: 8, maximum: 64 }, presence: true
 
+    # TODO: encrypt password
 end
